0aec9485660e26d790dc1328df2917b59a6aca4d
Temur Esanov <temuresanov01@gmail.com>
2022-04-21 07:53:24 +0300

    add schema

---
+++ models/users.js
@@ -0,0 +1,29 @@
+const mongoose = require ('mongoose')
+const { Schema, model } = mongoose;
+
+const usersSchema = new Schema({
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    subscription: {
+        type: String,
+        enum: ["starter", "pro", "business"],
+        default: "starter"
+    },
+    token: {
+        type: String,
+        default: null,
+    },
+});
+
+const User = model('contact', usersSchema)
+
+module.exports = {
+  User,
+}
